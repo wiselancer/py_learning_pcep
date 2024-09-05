f85d2aaf027fc72ed75ed280e1c6ae5e5a419ac2
Wiselancer <310760+wiselancer@users.noreply.github.com>
2024-09-05 15:35:40 +0800

    Completed 03.ASCII

---
+++ excercises/03.ASCII.py
@@ -0,0 +1,17 @@
+# Python code​​​​​​‌​‌‌‌​‌​‌‌​​​​‌​‌‌​‌​‌​‌​ below
+import itertools
+
+def encodeString(stringVal):
+    # Your code goes here.
+    result = []
+    for char, group in itertools.groupby(stringVal):
+        count = len(list(group))
+        result.append((char, count))
+    return result
+
+def decodeString(encodedList):
+    # Your code goes here.
+    result = ''
+    for k, v in encodedList:
+        result += k*v
+    return result
